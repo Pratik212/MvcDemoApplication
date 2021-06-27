08ce5c28daaad68beba4e8af5573b70f8882ba60
Pratik <pratik@ghanshyamdigital.com>
2021-06-27 14:07:43 +0530

    Add project files.

---
+++ DemoApplication/Controllers/ItemController.cs
@@ -0,0 +1,45 @@
+﻿using DemoApplication.Data;
+using DemoApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApplication.Controllers
+{
+    public class ItemController : Controller
+    {
+
+        private readonly DemoContext _context;
+
+        public ItemController(DemoContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _context.Items;
+
+            return View(objList);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //POST-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item obj)
+        {
+            _context.Items.Add(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DemoApplication/Data/DemoContext.cs
@@ -0,0 +1,18 @@
+﻿using DemoApplication.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApplication.Data
+{
+    public class DemoContext : DbContext
+    {
+        public DemoContext(DbContextOptions<DemoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
